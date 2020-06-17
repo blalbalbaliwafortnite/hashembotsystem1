9cb984540776cd5bb6038b36e3f2eb300d5668bf
blalbalbaliwafortnite <66137680+blalbalbaliwafortnite@users.noreply.github.com>
2020-06-17 02:08:55 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  prefix: "!", 
+  token: "NzIwNzI5MjAxOTYzMDQwODkw.Xulj2g.pcGbQvzOCat9LnTm6gU-yd_o3g8",
+  apiKey: "",
+  devs: ["406127752484487168"], ///تعديل اساسي ايدي حسابك
+  channelcreator: ["715579681948303464"],
+};
