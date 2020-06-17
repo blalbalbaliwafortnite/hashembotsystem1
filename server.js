ebf57faf9189e32101e5ca4804aae353ecbe264f
blalbalbaliwafortnite <66137680+blalbalbaliwafortnite@users.noreply.github.com>
2020-06-17 17:12:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -208,6 +208,59 @@ client.on('message', message => {
 });
 
 
+
+
+
+client.on('message', msg => {
+
+if(msg.content.startsWith(prefix+"reaction")){
+  if(!msg.channel.guild) return;
+  for(let n in emojiname){
+  var emoji =[msg.guild.emojis.find(r => r.name == emojiname[n])];
+  for(let i in emoji){
+   msg.react(emoji[i]);
+  }
+ }
+}
+});
+
+var emojiname = ["✔️","⭕"];
+
+//    Add role name
+var rolename=["PC","Mobile"];
+
+client.on("messageReactionAdd",(reaction,user)=>{
+  if(!user) return;
+  if(user.bot)return;
+  if(!reaction.message.channel.guild) return;
+  for(let n in emojiname){
+  if(reaction.emoji.name == emojiname[n]){
+    let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);          
+    reaction.message.guild.member(user).addRole(role).catch(console.error);
+  }
+}
+});
+
+
+client.on("messageReactionRemove",(reaction,user)=>{
+  if(!user) return;
+  if(user.bot)return;
+  if(!reaction.message.channel.guild) return;
+  for(let n in emojiname){
+  if(reaction.emoji.name == emojiname[n]){
+    let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);   
+    reaction.message.guild.member(user).removeRole(role).catch(console.error);
+  }
+  }
+});
+
+
+
+
+
+
+
+
 client.on('message', message => {
     if (message.channel.id === '718209175783735378') {
     let messageArray = message.content.split(" ");    
