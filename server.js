02256f2e564efee131aab2903ef26b22ae3c19bb
blalbalbaliwafortnite <66137680+blalbalbaliwafortnite@users.noreply.github.com>
2020-06-17 15:12:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -702,6 +702,11 @@ setInterval(() => {
     message.channel.sendMessage("دور هنا علي الناس تبغى تلعب معاهم مو لازم تكتب ب شات العام" + message.author)
     message.channel.sendMessage("<#718190508354175158> <#718187163082031177> <#716295431327580250> <#718187464111685695> <#716268576587120730>")
   }
+  if (message.content.includes("تلعب معي")) {
+    message.delete(1);
+    message.channel.sendMessage("دور هنا علي الناس تبغى تلعب معاهم مو لازم تكتب ب شات العام" + message.author)
+    message.channel.sendMessage("<#718190508354175158> <#718187163082031177> <#716295431327580250> <#718187464111685695> <#716268576587120730>")
+  }
     } else { 
     }
 }
