e7194e6b370aebc09803c32cdad5c150f2cf3688
blalbalbaliwafortnite <66137680+blalbalbaliwafortnite@users.noreply.github.com>
2020-06-21 21:28:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   prefix: "!", 
-  token: "NzIwNzI5MjAxOTYzMDQwODkw.Xuqbbg.dm-zK6PsoFm4Zju3CjDhejCh1bo",
+  token: "NzIwNzI5MjAxOTYzMDQwODkw.Xu_CpA.Zoe27IiuPkTC8Yu8gl63zt1ZyeI",
   apiKey: "",
   devs: ["406127752484487168"], ///تعديل اساسي ايدي حسابك
   channelcreator: ["715579681948303464"],
