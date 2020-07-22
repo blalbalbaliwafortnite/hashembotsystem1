3afe0583359d7e201fe10151f8ceaf6c7b12ccad
blalbalbaliwafortnite <66137680+blalbalbaliwafortnite@users.noreply.github.com>
2020-07-22 04:48:57 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -12,21 +12,21 @@ module.exports.run = async (bot, message, args) => {
 
         // No person found
         if (!rMember)
-            return message.reply("Couldn't find that person?").then(m => m.delete(5000));
+            return message.reply("لايمكنني ايجاد الشخص").then(m => m.delete(5000));
 
         // The member has BAN_MEMBERS or is a bot
         if (rMember.hasPermission("BAN_MEMBERS") || rMember.user.bot)
-            return message.channel.send("Can't report that member").then(m => m.delete(5000));
+            return message.channel.send("لايمكنك ابلاغ علي آداره او المشرفين.").then(m => m.delete(5000));
 
         // If there's no argument
         if (!args[1])
-            return message.channel.send("Please provide a reason for the report").then(m => m.delete(5000));
+            return message.channel.send("يرجى كتابة السبب حول ابلاغ علي العضو").then(m => m.delete(5000));
         
         const channel = message.guild.channels.find(c => c.name === "logs-reports")
             
         // No channel found
         if (!channel)
-            return message.channel.send("Couldn't find a `#reports` channel").then(m => m.delete(5000));
+            return message.channel.send("لايمكنني ايجاد القناة الريبورت يرجى انشاء قناة").then(m => m.delete(5000));
 
         const embed = new Discord.RichEmbed()
             .setColor("#ff0000")
